85b60868c002ea69be892dab5a0e5261776e0c7c
Marcos Morais <moraismarcos727@gmail.com>
2024-02-05 11:53:51 -0300

    A

--- ferramentas/Visualizacao_em_grupo.py
+++ ferramentas/Visualizacao_em_grupo.py
@@ -60,7 +60,7 @@ for caminhos_arquivos in lista:
                 title_fontsize=tamanho_fonte
             )
 
-            plt.savefig(f'TESTES/{caminho[1]}/GRAFICOS/{caminho[4]}_accuracy.jpg')
+            plt.savefig(f'TESTES/{caminho[1]}/GRAFICOS/{caminho[4]}_accuracy.png')
             plt.close('all')
 
             plt.figure(figsize=(9, 5))
@@ -87,7 +87,7 @@ for caminhos_arquivos in lista:
                 title_fontsize=tamanho_fonte
             )
 
-            plt.savefig(f'TESTES/{caminho[1]}/GRAFICOS/{caminho[4]}_loss.jpg')
+            plt.savefig(f'TESTES/{caminho[1]}/GRAFICOS/{caminho[4]}_loss.png')
             plt.close('all')
         except Exception as e:
             print(f"Ocorreu um erro ao processar o arquivo {arquivo}: {str(e)}")
--- ferramentas/gera_grafico_margem_de_erro.py
+++ ferramentas/gera_grafico_margem_de_erro.py
@@ -6,7 +6,7 @@ import numpy as np
 import glob
 
 sns.set_theme(style='darkgrid')
-
+tamanho_fonte = 25
 lista = []
 try:
     niid_iid = ['IID', 'NIID']
@@ -20,6 +20,7 @@ try:
 
 except Exception as e:
     print(f"Ocorreu um erro ao processar: {str(e)}")
+
 for caminhos_arquivos in lista:
     rotulos = []
     for arquivo in caminhos_arquivos:
@@ -32,13 +33,27 @@ for caminhos_arquivos in lista:
             rotulo = f'{base[-1]}'
             rotulos.append(rotulo)
 
+            plt.figure(figsize=(9, 5))
             for i, arquivo_atual in enumerate(caminhos_arquivos):
                 data = pd.read_csv(arquivo_atual, header=None)
                 data.columns = ['server_round', 'cid', 'accuracy', 'loss']
 
-                sns.lineplot(x='server_round', y='accuracy',
-                data=data)
-            plt.savefig(f'TESTES/{caminho[1]}/GRAFICOS/{caminho[4]}_accuracy.jpg')
+                sns.lineplot(x='server_round', y='accuracy', data=data)
+
+            xticks = np.arange(0,101,10)
+            plt.xticks(xticks, fontsize=tamanho_fonte)
+            plt.xticks(fontsize=tamanho_fonte)
+            plt.yticks(fontsize=tamanho_fonte)
+
+            plt.grid(color='k', linestyle='--', linewidth=0.5, axis='both', alpha=0.1)
+            plt.legend(
+                loc='best',
+                fontsize=tamanho_fonte,
+                ncol=1,
+                title='# Round',
+                title_fontsize=tamanho_fonte
+            )
+            plt.savefig(f'TESTES/{caminho[1]}/GRAFICOS/{caminho[4]}_accuracy.png', dpi=300)
             plt.close('all')
         except Exception as e:
             print(f"Ocorreu um erro ao processar o arquivo {arquivo}: {str(e)}")
@@ -46,4 +61,4 @@ for caminhos_arquivos in lista:
 
 
 
-plt.show()
+# plt.show()
