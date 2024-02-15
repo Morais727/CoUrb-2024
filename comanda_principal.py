26ba135d82ff126b0651e75a61a9d8db24860395
Marcos Morais <moraismarcos727@gmail.com>
2024-02-15 18:11:42 -0300

    A

--- comanda_principal.py
+++ comanda_principal.py
@@ -33,13 +33,17 @@ arquivos_teste= [
 def executar_arquivo(arquivo):
 
     try:
-        niid_iid = ['IID', 'NIID']
-        ataques = ['ALTERNA_INICIO', 'ATACANTES', 'EMBARALHA', 'INVERTE_TREINANDO', 'INVERTE_SEM_TREINAR', 'INVERTE_CONVEGENCIA', 'ZEROS', 'RUIDO_GAUSSIANO', 'NORMAL']
-        data_set = ['MNIST', 'CIFAR10']
         modelos = ['DNN', 'CNN']
-        variaveis = [2, 4, 6, 8]
+        niid_iid = ['IID', 'NIID']        
+        ataques = ['ALTERNA_INICIO', 'ATACANTES', 'EMBARALHA', 'INVERTE_TREINANDO', 'INVERTE_SEM_TREINAR', 'INVERTE_CONVEGENCIA', 'ZEROS', 'RUIDO_GAUSSIANO', 'NORMAL']
+        data_set = ['MNIST', 'CIFAR10']                        
+        alpha_dirichlet = [0.1,0.5,2,5,10]
+        noise_gaussiano = [0.1,0.5,0.8]
+        round_inicio = [2, 4, 6, 8]
+        per_cents_atacantes = [30,60,90]
+        
 
-        for i, j, k, l, m in product(niid_iid, ataques, data_set, modelos, variaveis):
+        for i, j, k, l, m, n, o, p in product(niid_iid, ataques, data_set, modelos, round_inicio, per_cents_atacantes, noise_gaussiano, alpha_dirichlet):
             print(f'Executando {arquivo}')
             
             if k == 'MNIST' and l == 'CNN':
@@ -49,7 +53,7 @@ def executar_arquivo(arquivo):
                 print(f'Combinação inválida: Dataset {k} com modelo {l}. Pulando execução.')
                 continue
             
-            comando = f'python3 {arquivo} --iid_niid {i} --modo_ataque {j} --dataset {k} --modelo_definido {l} --variavel {m}'
+            comando = f'python3 {arquivo} --iid_niid {i} --modo_ataque {j} --dataset {k} --modelo_definido {l} --round_inicio {m} --per_cents_atacantes {n} --noise_gaussiano {o} --alpha_dirichlet {p}'
             print(f'\n\n################################################################################################')
             print(f'\n\n{comando}\n\n')
             print(f'################################################################################################\n\n')
