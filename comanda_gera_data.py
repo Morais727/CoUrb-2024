fda17cd241d521e32a883d203986fbf15eb1817d
Marcos Morais <moraismarcos727@gmail.com>
2024-03-01 14:08:35 -0300

    A

--- comanda_gera_data.py
+++ comanda_gera_data.py
@@ -15,7 +15,7 @@ def executar_arquivo(arquivo):
         niid_iid = ['NIID']        
         ataques = ['ALTERNA_INICIO', 'ATACANTES', 'EMBARALHA', 'INVERTE_TREINANDO', 'INVERTE_SEM_TREINAR', 'INVERTE_CONVEGENCIA', 'ZEROS', 'RUIDO_GAUSSIANO', 'NORMAL']
         data_set = ['MNIST', 'CIFAR10']                        
-        alpha_dirichlet = [0.0]
+        alpha_dirichlet = [0.0, 0.1, 0.2, 0.5, 1]
         noise_gaussiano = [0.0, 0.1, 0.5, 0.8]
         round_inicio = [2, 4, 6, 8]
         per_cents_atacantes = [30, 60, 80, 85, 88, 90, 95]
@@ -25,16 +25,20 @@ def executar_arquivo(arquivo):
 
         for i, j, k, l, m, n, o, p, q, r, s in product(niid_iid, ataques, data_set, modelos, round_inicio, per_cents_atacantes, noise_gaussiano, alpha_dirichlet, num_round, modo_execucao, total_clients):
             combinacao = (i, j, k, l, m, n, o, p, q, r, s) 
-            if i == 'NIID' and p == 0:                
+            if i == 'NIID' and p == 0: 
+                print('NON IID com Dirichlet = 0')               
                 continue
 
-            if i == 'IID' and p > 0:                
+            if i == 'IID' and p > 0: 
+                print('IID com Dirichlet > 0', i, p)                
                 continue
 
             if j != 'RUIDO_GAUSSIANO' and o > 0:
+                print('RUIDO_GAUSSIANO > 0', j, p)   
                 continue
 
             if (k == 'MNIST' and l == 'CNN') or (k == 'CIFAR10' and l == 'DNN'):
+                print("Combinacao incorreta", k, l)
                 continue
 
             if combinacao not in combinacoes_unicas:                  
--- comanda_principal_iid.py
+++ comanda_principal_iid.py
@@ -51,13 +51,20 @@ def executar_arquivo(arquivo):
         for i, j, k, l, m, n, o, p, q, r, s in product(niid_iid, ataques, data_set, modelos, round_inicio, per_cents_atacantes, noise_gaussiano, alpha_dirichlet, num_round, modo_execucao, total_clients):
             combinacao = (i, j, k, l, m, n, o, p, q, r, s) 
             
-            if i == 'IID' and p > 0:                
+            if i == 'NIID' and p == 0: 
+                print('NON IID com Dirichlet = 0')               
+                continue
+
+            if i == 'IID' and p > 0: 
+                print('IID com Dirichlet > 0', i, p)                
                 continue
 
             if j != 'RUIDO_GAUSSIANO' and o > 0:
+                print('RUIDO_GAUSSIANO > 0', j, p)   
                 continue
 
             if (k == 'MNIST' and l == 'CNN') or (k == 'CIFAR10' and l == 'DNN'):
+                print("Combinacao incorreta", k, l)
                 continue
 
             if combinacao not in combinacoes_unicas:                  
