e92abed2e6c972e72a84fcd8157a5ea091169114
Marcos Morais <moraismarcos727@gmail.com>
2024-03-01 13:29:30 -0300

    A

--- comanda_principal_iid.py
+++ comanda_principal_iid.py
@@ -65,9 +65,7 @@ def executar_arquivo(arquivo):
 
             print(f'Executando {arquivo}')                
             comando = f'python3 {arquivo} --iid_niid {i} --modo_ataque {j} --dataset {k} --modelo_definido {l} --round_inicio {m} --per_cents_atacantes {n} --noise_gaussiano {o} --alpha_dirichlet {p} --num_rounds {q} --modo_execucao {r}'
-        
-            print(f'Executando {arquivo}')                
-            comando = f'python3 {arquivo} --iid_niid {i} --modo_ataque {j} --dataset {k} --modelo_definido {l} --round_inicio {m} --per_cents_atacantes {n} --noise_gaussiano {o} --alpha_dirichlet {p}'
+                    
             print(f'\n\n################################################################################################')
             print(f'\n\n{comando}\n\n')
             print(f'################################################################################################\n\n')
