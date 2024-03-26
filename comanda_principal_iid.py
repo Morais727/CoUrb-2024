fdfc43842be0cf5f6e7e83bdc2bddda04ef693d7
Marcos Morais <moraismarcos727@gmail.com>
2024-03-25 22:31:58 -0300

    A

--- comanda_principal_iid.py
+++ comanda_principal_iid.py
@@ -35,7 +35,7 @@ def executar_arquivo(arquivo):
         niid_iid = ['IID']        
         ataques = ['ALTERNA_INICIO', 'ATACANTES', 'EMBARALHA', 'INVERTE_TREINANDO', 'INVERTE_SEM_TREINAR', 'INVERTE_CONVEGENCIA', 'ZEROS', 'RUIDO_GAUSSIANO', 'NORMAL']
         data_set = ['MNIST', 'CIFAR10']                        
-        alpha_dirichlet = [0.0,0.1]
+        alpha_dirichlet = [0.0]
         noise_gaussiano = [0.1,0.0]
         round_inicio = [4]
         per_cents_atacantes = [40]
