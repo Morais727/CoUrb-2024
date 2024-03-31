af322bff84b607cbb9d70f270354fa8a2cc35e03
Marcos Morais <moraismarcos727@gmail.com>
2024-03-31 20:50:21 -0300

    A

--- comanda_principal_iid.py
+++ comanda_principal_iid.py
@@ -27,7 +27,7 @@ def executar_arquivo(arquivo):
         num_round = [20]
         total_clients = [20]
         modelos = ['CNN','DNN']
-        niid_iid = ['IID']        
+        niid_iid = ['IID','NIID']        
         ataques = ['ALTERNA_INICIO', 'ATACANTES', 'EMBARALHA', 'INVERTE_TREINANDO', 'INVERTE_SEM_TREINAR', 'INVERTE_CONVEGENCIA', 'ZEROS', 'RUIDO_GAUSSIANO', 'NORMAL']
         data_set = ['MNIST', 'CIFAR10']                        
         alpha_dirichlet = [0.0,0.5]
@@ -87,63 +87,6 @@ def executar_arquivo(arquivo):
     except Exception as e:
         print(f'Erro inesperado: {e}')
 
-arquivos_data = ['simulacao_principal_data.py']
-
-def executar_arquivo_data(arquivo):
-    try:
-        num_round = [20]
-        total_clients = [20]
-        modelos = ['DNN','CNN']
-        niid_iid = ['IID']        
-        ataques = ['ALTERNA_INICIO', 'ATACANTES', 'EMBARALHA', 'INVERTE_TREINANDO', 'INVERTE_SEM_TREINAR', 'INVERTE_CONVEGENCIA', 'ZEROS', 'RUIDO_GAUSSIANO', 'NORMAL']
-        data_set = ['MNIST', 'CIFAR10']                        
-        alpha_dirichlet = [0.0,0.1,0.3]
-        noise_gaussiano = [0.1,0.0]
-        round_inicio = [2,8]
-        per_cents_atacantes = [20]
-
-        combinacoes_unicas = set() 
-
-        for i, j, k, l, m, n, o, p, q, r in product(niid_iid, ataques, data_set, modelos, round_inicio, per_cents_atacantes, noise_gaussiano, alpha_dirichlet, num_round, total_clients):
-            combinacao = (i, j, k, l, m, n, o, p, q, r) 
-            
-            if i == 'NIID' and p == 0: 
-                print('NON IID com Dirichlet = 0')               
-                continue
-
-            if i == 'IID' and p > 0: 
-                print('IID com Dirichlet > 0', i, p)                
-                continue
-
-            if j != 'RUIDO_GAUSSIANO' and o > 0:
-                print('RUIDO_GAUSSIANO > 0', j, p)   
-                continue
-            elif j == 'RUIDO_GAUSSIANO' and o != 0:
-                continue
-
-            if (k == 'MNIST' and l == 'CNN') or (k == 'CIFAR10' and l == 'DNN'):
-                print("Combinacao incorreta", k, l)
-                continue
-
-            if combinacao not in combinacoes_unicas:                  
-                combinacoes_unicas.add(combinacao)
-            else:
-                continue 
-
-            print(f'Executando {arquivo} GERANDO DATA')                
-            comando = f'python3 {arquivo} --iid_niid {i} --modo_ataque {j} --dataset {k} --modelo_definido {l} --round_inicio {m} --per_cents_atacantes {n} --noise_gaussiano {o} --alpha_dirichlet {p} --num_rounds {q}  --total_clients {r}'
-                    
-            print(f'\n\n################################################################################################')
-            print(f'\n\n{comando}\n\n')
-            print(f'################################################################################################\n\n')
-            subprocess.run(shlex.split(comando), check=True)
-
-            print(f'Executou com sucesso: {arquivo}')
-
-    except subprocess.CalledProcessError as e:
-        print(f'Erro ao executar o arquivo: {e}')
-    except Exception as e:
-        print(f'Erro inesperado: {e}')
 
 def processar_dados(arquivos):
     for arquivo in arquivos:
@@ -152,15 +95,8 @@ def processar_dados(arquivos):
         except subprocess.CalledProcessError as e:
             print(f"Erro ao executar {arquivo}: {e}")
 
-arquivos_dados = ['ferramentas/conta.py', 'ferramentas/XGB_CLASSIFIER.py']
-
 
 max_threads = 1
-for i in range(70):
-    with concurrent.futures.ThreadPoolExecutor(max_threads) as executor:
-        resultados = list(executor.map(executar_arquivo_data, arquivos_data))
-
-processar_dados(arquivos_dados)
 
 with concurrent.futures.ThreadPoolExecutor(max_threads) as executor:
     resultados = list(executor.map(executar_arquivo, arquivos_teste))
